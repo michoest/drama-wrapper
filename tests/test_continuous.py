741caf001632965e072f90c0daed7124e83a76b2
Tim Grams <t.grams339@gmail.com>
2023-05-24 12:16:16 +0200

    transfered to pettingzoo and removed old files

--- tests/test_continuous.py
+++
@@ -1,8 +0,0 @@
-from unittest import TestCase
-
-
-class TestContinuous(TestCase):
-
-    def test_imports(self):
-        from src.spaces.bucket_space import BucketSpace
-        from src.spaces.interval_union import IntervalUnionTree
