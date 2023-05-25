d3b3e3d088fb3d49fc524d580ce18289c61ca576
Michael Oesterle <michaeloesterle01@gmail.com>
2023-05-26 01:06:28 +0200

    Add variable keys and minor improvements for RestrictionWrapper

--- src/wrapper.py
+++ src/wrapper.py
@@ -7,43 +7,49 @@ from pettingzoo.utils import BaseWrapper
 
 
 class RestrictionWrapper(BaseWrapper):
-    """ Wrapper to extend the environment with a governance agent.
+    """ Wrapper to extend the environment with a restrictor agent.
 
      Extended Agent-Environment Cycle:
-         Reset() -> Governance
+         Reset() -> Restrictor
          Step() -> Agent
-         Step() -> Governance
+         Step() -> Restrictor
          ...
      """
 
-    def __init__(self, env, governance_observation_space, governance_action_space,
-                 governance_reward_fn=None, preprocess_governance_observation_fn=None):
+    def __init__(self, env, restrictor_observation_space, restrictor_action_space, *,
+                 restrictor_reward_fn=None, preprocess_restrictor_observation_fn=None,
+                 restrictor_key='restrictor_0', restriction_key='restriction', 
+                 observation_key='observation'):
         super().__init__(env)
 
-        self.governance_observation_space = governance_observation_space
-        self.governance_action_space = governance_action_space
-        self.governance_reward_fn = governance_reward_fn
-        self.preprocess_governance_observation_fn = preprocess_governance_observation_fn
+        self.restrictor_observation_space = restrictor_observation_space
+        self.restrictor_action_space = restrictor_action_space
+        self.restrictor_reward_fn = restrictor_reward_fn
+        self.preprocess_restrictor_observation_fn = preprocess_restrictor_observation_fn
+
+        self.restrictor_key = restrictor_key
+        self.restriction_key = restriction_key
+        self.observation_key = observation_key
 
         # self.observations and self.restrictions are dictionaries which keep the latest values for each agent
         self.observations = None
         self.restrictions = None
-        self.possible_agents = ['gov_0'] + self.possible_agents
+        self.possible_agents = [self.restrictor_key] + self.possible_agents
 
     @functools.lru_cache(maxsize=None)
     def observation_space(self, agent):
-        if agent == 'gov_0':
-            return self.governance_observation_space
+        if agent == self.restrictor_key:
+            return self.restrictor_observation_space
         else:
             return Dict({
-                'observation': self.env.observation_space(agent),
-                'restriction': self.governance_action_space
+                self.observation_key: self.env.observation_space(agent),
+                self.restriction_key: self.restrictor_action_space
             })
 
     @functools.lru_cache(maxsize=None)
     def action_space(self, agent):
-        if agent == 'gov_0':
-            return self.governance_action_space
+        if agent == self.restrictor_key:
+            return self.restrictor_action_space
         else:
             return self.env.action_space(agent)
 
@@ -53,20 +59,20 @@ class RestrictionWrapper(BaseWrapper):
         self.observations = {agent: None for agent in self.env.possible_agents}
         self.restrictions = {agent: None for agent in self.env.possible_agents}
 
-        # Then update the governance attributes
+        # Then update the restrictor attributes
         self.agents = copy(self.possible_agents)
-        self.rewards['gov_0'] = 0.0
-        self._cumulative_rewards['gov_0'] = 0.0
-        self.terminations['gov_0'] = False
-        self.truncations['gov_0'] = False
-        self.infos['gov_0'] = {}
+        self.rewards[self.restrictor_key] = 0.0
+        self._cumulative_rewards[self.restrictor_key] = 0.0
+        self.terminations[self.restrictor_key] = False
+        self.truncations[self.restrictor_key] = False
+        self.infos[self.restrictor_key] = {}
 
-        # Start an episode with the governance to obtain restrictions
-        self.agent_selection = 'gov_0'
+        # Start an episode with the restrictor to obtain restrictions
+        self.agent_selection = self.restrictor_key
 
     def step(self, action):
-        if self.agent_selection == 'gov_0':
-            # If the action was taken by the governance, check if it was terminated last step
+        if self.agent_selection == self.restrictor_key:
+            # If the action was taken by the restrictor, check if it was terminated last step
             if self.terminations[self.agent_selection] or self.truncations[self.agent_selection]:
                 self._was_dead_step(action)
                 self.agent_selection = self.env.agent_selection
@@ -81,29 +87,30 @@ class RestrictionWrapper(BaseWrapper):
             # If the action was taken by an agent, execute it in the original environment
             super().step(action)
 
-            # Only setup the governance for the next cycle if there are agents left
-            if len(self.agents) >= 1:
-                # If more restrictions are required, configure the governance
-                # If all agents are now terminated or truncated then also terminate the governance
-                self.agents = ['gov_0'] + self.env.agents
-                self.terminations['gov_0'] = True \
+            # Only setup the restrictor for the next cycle if there are agents left
+            if self.agents:
+                # If more restrictions are required, configure the restrictor
+                # If all agents are now terminated or truncated then also terminate the restrictor
+                self.agents = [self.restrictor_key] + self.env.agents
+                print(f'{self.terminations=}, {self.truncations=}')
+                self.truncations[self.restrictor_key] = False
+                self.terminations[self.restrictor_key] = True \
                     if np.all(np.logical_or(np.array(list(self.terminations.values())),
                                             np.array(list(self.truncations.values())))) else False
-                self.truncations['gov_0'] = False
-                self.infos['gov_0'] = {}
-                self.rewards['gov_0'] = self.governance_reward_fn(self.rewards, self.env.state()) \
-                    if self.governance_reward_fn else sum(self.rewards.values())
-                self._cumulative_rewards['gov_0'] += self.rewards['gov_0']
+                self.infos[self.restrictor_key] = {}
+                self.rewards[self.restrictor_key] = self.restrictor_reward_fn(self.rewards, self.env) \
+                    if self.restrictor_reward_fn else sum(self.rewards.values())
+                self._cumulative_rewards[self.restrictor_key] += self.rewards[self.restrictor_key]
 
-                # Switch back to the governance
-                self.agent_selection = 'gov_0'
+                # Switch back to the restrictor
+                self.agent_selection = self.restrictor_key
 
     def observe(self, agent: str):
-        if agent == 'gov_0':
-            return self.preprocess_governance_observation_fn(self.observations, self.env.state()) \
-                if self.preprocess_governance_observation_fn else self.env.state()
+        if agent == self.restrictor_key:
+            return self.preprocess_restrictor_observation_fn(self.env, self.env.agent_selection, self.observations) \
+                if self.preprocess_restrictor_observation_fn else self.env
         else:
             return {
-                'observation': super().observe(agent),
-                'restriction': self.restrictions[agent]
+                self.observation_key: super().observe(agent),
+                self.restriction_key: self.restrictions[agent]
             }
