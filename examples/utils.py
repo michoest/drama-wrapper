1109ac177eebf0cfea45759da9972012c3eb4503
Michael Oesterle <michaeloesterle01@gmail.com>
2023-06-05 16:54:01 +0200

    Return trajectory from play()

--- examples/utils.py
+++ examples/utils.py
@@ -1,6 +1,9 @@
-def play(env, policies, *, max_iter=1_000, verbose=False):
+def play(env, policies, *, max_iter=1_000, verbose=False, record_trajectory=False):
     env.reset()
     env.render()
+
+    if record_trajectory:
+        trajectory = []
 
     for i, agent in zip(range(max_iter), env.agent_iter()):
         observation, reward, termination, truncation, info = env.last()
@@ -16,9 +19,14 @@ def play(env, policies, *, max_iter=1_000, verbose=False):
         if verbose:
             print(f"{action=}")
 
+        if record_trajectory:
+            trajectory.append((agent, observation, reward, termination, truncation, info, action))
+
         env.step(action)
 
     env.render()
+
+    return trajectory if record_trajectory else None
 
 
 def restriction_aware_random_policy(observation):
