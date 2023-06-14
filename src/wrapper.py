e180446aaa2ce169162e532b285129cd0b20ec18
Michael Oesterle <michaeloesterle01@gmail.com>
2023-06-14 16:18:37 +0200

    Clean up wrapper class

--- src/wrapper.py
+++ src/wrapper.py
@@ -1,10 +1,15 @@
-import functools
-from typing import Union, Callable
+# Typing
+from typing import Union, Callable, Optional
 
+# Standard modules
+import functools
+
+# External modules
 from gymnasium.spaces import Dict
 from pettingzoo import AECEnv
 from pettingzoo.utils import BaseWrapper
 
+# Internal modules
 from src.restrictions import Restriction
 from src.restrictors import Restrictor
 from src.utils import flatten
@@ -40,75 +45,106 @@ class RestrictionWrapper(BaseWrapper):
     """
 
     def __init__(
-            self,
-            env: AECEnv,
-            restrictors: Union[dict, Restrictor],
-            *,
-            agent_restrictor_mapping: dict = None,
-            restrictor_reward_fns: Union[dict, Callable] = None,
-            preprocess_restrictor_observation_fns: Union[dict, Callable] = None,
-            postprocess_restriction_fns: Union[dict, Callable] = None,
-            restriction_violation_fns: Union[dict, Callable] = None,
-            restriction_key: str = "restriction",
-            observation_key: str = "observation",
-            return_object: bool = False,
-            **kwargs
+        self,
+        env: AECEnv,
+        restrictors: Union[dict, Restrictor],
+        *,
+        agent_restrictor_mapping: Optional[dict] = None,
+        restrictor_reward_fns: Union[dict, Callable, None] = None,
+        preprocess_restrictor_observation_fns: Union[dict, Callable] = None,
+        postprocess_restriction_fns: Union[dict, Callable] = None,
+        restriction_violation_fns: Union[dict, Callable] = None,
+        restriction_key: str = "restriction",
+        observation_key: str = "observation",
+        return_object: bool = False,
+        **kwargs,
     ):
-        if isinstance(restrictors, dict):
-            assert agent_restrictor_mapping, 'Agent-restrictor mapping required!'
         super().__init__(env)
 
-        self.restrictors = restrictors if isinstance(restrictors, dict) else {
-            'restrictor_0': restrictors
-        }
-        self.agent_restrictor_mapping = agent_restrictor_mapping if isinstance(restrictors, dict) else {
-            agent: 'restrictor_0' for agent in self.env.possible_agents}
+        if isinstance(restrictors, dict):
+            assert agent_restrictor_mapping, "Agent-restrictor mapping required!"
 
-        self.restrictor_reward_fns = {
-            restrictor: restrictor_reward_fns[restrictor]
-            if restrictor_reward_fns and restrictor_reward_fns.get(restrictor, None)
-            else _default_restrictor_reward_fn
-            for restrictor in self.restrictors
-        } if isinstance(restrictor_reward_fns, Union[dict, None]) else {
-            restrictor: restrictor_reward_fns for restrictor in self.restrictors}
+        self.restrictors = (
+            restrictors
+            if isinstance(restrictors, dict)
+            else {"restrictor_0": restrictors}  # Naming convention from PettingZoo
+        )
+        self.agent_restrictor_mapping = (
+            agent_restrictor_mapping
+            if isinstance(restrictors, dict)
+            else {agent: "restrictor_0" for agent in self.env.possible_agents}
+        )
+
+        self.restrictor_reward_fns = (
+            {
+                restrictor: restrictor_reward_fns[restrictor]
+                if restrictor_reward_fns and restrictor_reward_fns.get(restrictor, None)
+                else _default_restrictor_reward_fn
+                for restrictor in self.restrictors
+            }
+            if isinstance(restrictor_reward_fns, Union[dict, None])
+            else {restrictor: restrictor_reward_fns for restrictor in self.restrictors}
+        )
 
         # Set restrictor observation preprocessing functions
         if isinstance(preprocess_restrictor_observation_fns, Callable):
             self.preprocess_restrictor_observation_fns = {
-                restrictor: preprocess_restrictor_observation_fns for restrictor in self.restrictors}
+                restrictor: preprocess_restrictor_observation_fns
+                for restrictor in self.restrictors
+            }
         else:
             self.preprocess_restrictor_observation_fns = {
-                restrictor: _default_preprocess_restrictor_observation_fn for restrictor in self.restrictors}
+                restrictor: _default_preprocess_restrictor_observation_fn
+                for restrictor in self.restrictors
+            }
             for name, restrictor in self.restrictors.items():
-                if isinstance(preprocess_restrictor_observation_fns, dict) \
-                        and preprocess_restrictor_observation_fns.get(name, None):
-                    self.preprocess_restrictor_observation_fns[name] = preprocess_restrictor_observation_fns[
-                        name]
-                elif hasattr(restrictor, 'preprocess_observation'):
-                    self.preprocess_restrictor_observation_fns[name] = restrictor.preprocess_observation
+                if isinstance(
+                    preprocess_restrictor_observation_fns, dict
+                ) and preprocess_restrictor_observation_fns.get(name, None):
+                    self.preprocess_restrictor_observation_fns[
+                        name
+                    ] = preprocess_restrictor_observation_fns[name]
+                elif hasattr(restrictor, "preprocess_observation"):
+                    self.preprocess_restrictor_observation_fns[
+                        name
+                    ] = restrictor.preprocess_observation
 
         # Set restriction postprocessing functions
         if isinstance(postprocess_restriction_fns, Callable):
             self.postprocess_restriction_fns = {
-                restrictor: postprocess_restriction_fns for restrictor in self.restrictors}
+                restrictor: postprocess_restriction_fns
+                for restrictor in self.restrictors
+            }
         else:
             self.postprocess_restriction_fns = {
-                restrictor: _default_postprocess_restriction_fn for restrictor in self.restrictors}
+                restrictor: _default_postprocess_restriction_fn
+                for restrictor in self.restrictors
+            }
             for name, restrictor in self.restrictors.items():
-                if isinstance(postprocess_restriction_fns, dict) \
-                        and postprocess_restriction_fns.get(name, None):
-                    self.postprocess_restriction_fns[name] = postprocess_restriction_fns[
-                        name]
-                elif hasattr(restrictor, 'postprocess_restriction'):
-                    self.postprocess_restriction_fns[name] = restrictor.postprocess_restriction
+                if isinstance(
+                    postprocess_restriction_fns, dict
+                ) and postprocess_restriction_fns.get(name, None):
+                    self.postprocess_restriction_fns[
+                        name
+                    ] = postprocess_restriction_fns[name]
+                elif hasattr(restrictor, "postprocess_restriction"):
+                    self.postprocess_restriction_fns[
+                        name
+                    ] = restrictor.postprocess_restriction
 
-        self.restriction_violation_fns = {
-            agent: restriction_violation_fns[agent]
-            if restriction_violation_fns and restriction_violation_fns.get(agent, None)
-            else _default_restriction_violation_fn
-            for agent in self.env.possible_agents
-        } if isinstance(restriction_violation_fns, Union[dict, None]) else {
-            agent: restriction_violation_fns for agent in self.env.possible_agents}
+        self.restriction_violation_fns = (
+            {
+                agent: restriction_violation_fns[agent]
+                if restriction_violation_fns
+                and restriction_violation_fns.get(agent, None)
+                else _default_restriction_violation_fn
+                for agent in self.env.possible_agents
+            }
+            if isinstance(restriction_violation_fns, Union[dict, None])
+            else {
+                agent: restriction_violation_fns for agent in self.env.possible_agents
+            }
+        )
 
         self.restriction_key = restriction_key
         self.observation_key = observation_key
@@ -120,13 +156,17 @@ class RestrictionWrapper(BaseWrapper):
 
         self.possible_agents = self.possible_agents + list(self.restrictors)
 
-        # Check if restrictor action spaces match agent action spaces
+        # Check if restrictor action spaces (after post-processing) match
+        # agent action spaces
         for agent in self.env.possible_agents:
-            assert self.restrictors[
+            restrictor = self.restrictors[self.agent_restrictor_mapping[agent]]
+            sample_restriction = self.postprocess_restriction_fns[
                 self.agent_restrictor_mapping[agent]
-            ].action_space.is_compatible_with(
-                env.action_space(agent)
-            ), f"The action space of {self.agent_restrictor_mapping[agent]} and {agent} are not compatible!"
+            ](restrictor.action_space.sample())
+
+            assert sample_restriction.base_space == env.action_space(
+                agent
+            ), f"The action spaces of {self.agent_restrictor_mapping[agent]} and {agent} are not compatible!"
 
     @functools.lru_cache(maxsize=None)
     def observation_space(self, agent):
@@ -197,19 +237,22 @@ class RestrictionWrapper(BaseWrapper):
 
             # Otherwise set the restrictions that apply to the next agent.
             assert (
-                    self.agent_restrictor_mapping[self.env.agent_selection]
-                    == self.agent_selection
+                self.agent_restrictor_mapping[self.env.agent_selection]
+                == self.agent_selection
             )
             # self.restrictions[self.env.agent_selection] = action
-            self.restrictions[self.env.agent_selection] = self.postprocess_restriction_fns[self.agent_selection](action)
+            self.restrictions[
+                self.env.agent_selection
+            ] = self.postprocess_restriction_fns[self.agent_selection](action)
 
             # Switch to the next agent of the original environment
             self.agent_selection = self.env.agent_selection
         else:
             # Check if the action violated the current restriction for the agent
             if action and not self.restrictions[self.agent_selection].contains(action):
-                self.restriction_violation_fns[self.agent_selection](self.env, action,
-                                                                     self.restrictions[self.agent_selection])
+                self.restriction_violation_fns[self.agent_selection](
+                    self.env, action, self.restrictions[self.agent_selection]
+                )
             else:
                 # If the action was taken by an agent, execute it in the original
                 # environment
@@ -257,8 +300,8 @@ class RestrictionWrapper(BaseWrapper):
             }
 
             if self.env.agents and all(
-                    self.env.terminations[agent] or self.env.truncations[agent]
-                    for agent in self.env.agents
+                self.env.terminations[agent] or self.env.truncations[agent]
+                for agent in self.env.agents
             ):
                 # If there are alive agents left, get the next restriction
                 self.agent_selection = self.env.agent_selection
@@ -272,11 +315,16 @@ class RestrictionWrapper(BaseWrapper):
         if agent in self.restrictors:
             return self.preprocess_restrictor_observation_fns[agent](self.env)
         else:
-            return_object = return_object if return_object is not None else self.return_object
+            return_object = (
+                return_object if return_object is not None else self.return_object
+            )
             return {
                 self.observation_key: super().observe(agent),
                 self.restriction_key: self.restrictions[agent]
                 if return_object and self.restrictions[agent].is_np_flattenable
-                else flatten(self.restrictors[self.agent_restrictor_mapping[agent]].action_space,
-                             self.restrictions[agent], **{**self.kwargs, **kwargs})
+                else flatten(
+                    self.restrictors[self.agent_restrictor_mapping[agent]].action_space,
+                    self.restrictions[agent],
+                    **{**self.kwargs, **kwargs},
+                ),
             }
